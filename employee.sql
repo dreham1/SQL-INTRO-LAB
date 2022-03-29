b25f660abca114380038be7038ef1ebf3efc1387
Deondre Hambric <dhambric1@gmail.com>
2022-03-28 21:38:14 -0600

    sec com

---
+++ employee.sql
@@ -0,0 +1,11 @@
+-- select * from employee
+-- #1
+-- select * from employee where city in ('Calgary')
+-- #2
+-- select max (birth_date) from employee
+-- #3
+-- select min (birth_date) from employee
+-- #4
+-- select * from employee where reports_to =2;
+-- #5
+-- Select Count(*) FROM employee WHERE city = 'Lethbridge'
